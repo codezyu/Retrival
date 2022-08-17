24f85ec2881ed7a11c2f98d8280981ffc0fc907c
codezyu <1954285529@qq.com>
2022-08-17 20:24:40 +0800

    新增模型代码

---
+++ Retrival/model/build.py
@@ -0,0 +1,3 @@
+from Retrival.model.backbone import backboneModel
+def buildModel():
+    return backboneModel();
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from Retrival.config.config import load_arg
+from Retrival.util.deviceInfo import get_free_device_ids
+from Retrival.model.build import buildModel
 if __name__=='__main__':
     arg=vars(load_arg())
-
+    #set model
+    model=buildModel()
+    #set multi gpu
+    deviceId=get_free_device_ids();
+    masterDevice=deviceId[0]
+    model.cuda(masterDevice)
